05d46ea71fe4058d3ae20aa695b29f80f6ceff4c
px-db <pxdb.id@gmail.com>
2024-03-08 11:00:39 +0700

    update for streamlit

--- stocksummary.py
+++ stocksummary.py
@@ -1,3 +1,10 @@
 import streamlit as st
-import Summary
+import Summary as s
+
+ss = s.Summary(mode='remote', start_date='2023')
+
+ss.dfs_days
+st.line_chart(ss.dfs_days['20231229']['Close'])
+
+
 
